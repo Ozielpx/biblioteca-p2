fe9c01cd78ddff7607059306a599d6223ba8396c
LAB34 <lab34@fatec.com>
2024-10-24 20:23:20 -0300

    ultima atualização

---
+++ src/main/java/com/biblioteca/locacao/Model/BookStatus.java
@@ -0,0 +1,5 @@
+package com.biblioteca.locacao.Model;
+
+public enum BookStatus {
+    AVAILABLE, BORROWED
+}
---
+++ src/main/java/com/biblioteca/locacao/Model/Status.java
@@ -0,0 +1,5 @@
+package com.biblioteca.locacao.Model;
+
+public enum Status {
+    ACTIVE, INACTIVE
+}
